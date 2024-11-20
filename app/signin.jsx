9239c875a9d152f30296a8a04966791669424c8b
lindsaybrose <lindsaybrocken@hotmail.com>
2024-11-20 11:33:28 +0000

    styling in progress old styling removed

--- app/signin.jsx
+++ app/signin.jsx
@@ -2,7 +2,6 @@ import { View, Text } from "react-native";
 import React, { useState, useContext } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Pressable, TextInput } from "react-native-gesture-handler";
-import { StyleSheet } from "react-native";
 import { getUserList } from "./api";
 import { LoggedInUserContext } from "./contexts/loggedInUser";
 import { Redirect } from "expo-router";
@@ -55,24 +54,3 @@ const signin = () => {
 };
 
 export default signin;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    width: "100%",
-  },
-  inputcontainer: {
-    flexDirection: "column",
-    width: "100%",
-    alignItems: "center",
-    pointerEvents: "auto",
-  },
-  input: {
-    flex: 1,
-    borderColor: "black",
-    borderWidth: 1,
-    borderRadius: 5,
-    alignItems: "center",
-    fontSize: 18,
-  },
-});
