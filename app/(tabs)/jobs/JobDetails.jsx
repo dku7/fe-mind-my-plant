be74a3b6fe2b3bdbeed4403bbfdf142ad9fd3495
lindsaybrose <lindsaybrocken@hotmail.com>
2024-11-21 15:29:34 +0000

    trying to link jobcard to job details

--- app/(tabs)/jobs/JobDetails.jsx
+++ app/(tabs)/jobs/JobDetails.jsx
@@ -1,5 +1,9 @@
-const JobDetails = () => {
+import React from "react"
+import { Text } from "react-native"
 
+const JobDetails = (props) => {
+
+return <Text>Job details</Text>
 }
 
 export default JobDetails
